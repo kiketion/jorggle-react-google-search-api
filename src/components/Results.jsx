1fa45d76db9204d8ff0b263f6f4a29f77c203f15
kiketion <kiketionje@gmail.com>
2022-07-16 20:35:36 +0300

    search

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -6,14 +6,38 @@ import { useResultContext } from '../contexts/ResultContexProvider';
 import Loading from './Loading';
 
 const Results = () => {
-  const { results, isLoading, getResluts, searchTerm } = useResultContext();
+  const { results, isLoading, getResults, searchTerm } = useResultContext();
   const location = useLocation();
 
+  useEffect(() => {
+    getResults('/search/q=Kiketion&num=40');
+  }, []);
+
   if (isLoading) return <Loading />;
-  console.log(location.pathname);
 
   switch (location.pathname) {
     case '/search':
+      return (
+        <div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
+          {results?.results?.map(({ link, title }, index) => (
+            <div key={index} className='md:w-2/5 w-full'>
+              <a href={link} target='_blank' rel='noreferrer'>
+                <p className='text-sm'>
+                  {link.length > 30 ? link.substring(0, 30) : link}
+                </p>
+                <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
+                  {title}
+                </p>
+              </a>
+            </div>
+          ))}
+        </div>
+      );
+    case '/images':
+      return 'SEARCH';
+    case '/news':
+      return 'SEARCH';
+    case '/videos':
       return 'SEARCH';
 
     default:
--- src/contexts/ResultContexProvider.js
+++ src/contexts/ResultContexProvider.js
@@ -2,6 +2,15 @@ import React, { createContext, useContext, useState } from 'react';
 
 const ResultContext = createContext();
 const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-User-Agent': 'desktop',
+    'X-Proxy-Location': 'EU',
+    'X-RapidAPI-Key': '18a47928a0msh9ea486c12af66c3p160543jsnaad846deb52c',
+    'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+  },
+};
 
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
@@ -12,17 +21,10 @@ export const ResultContextProvider = ({ children }) => {
   const getResults = async (type) => {
     setIsLoading(true);
 
-    const response = await fetch(`${baseUrl}${type}`, {
-      method: 'GET',
-      headers: {
-        'X-User-Agent': 'desktop',
-        'X-Proxy-Location': 'EU',
-        'X-RapidAPI-Key': '18a47928a0msh9ea486c12af66c3p160543jsnaad846deb52c',
-        'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
-      },
-    });
+    const response = await fetch(`${baseUrl}${type}`, options);
 
     const data = await response.json();
+
     setResults(data);
     setIsLoading(false);
   };
